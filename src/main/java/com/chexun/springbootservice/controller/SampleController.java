324a405a5978f067c5319d090640367f854858cd
impself <410917617@qq.com>
2017-12-07 23:53:12 +0800

    #0001# springboot-service新建

---
+++ src/main/java/com/chexun/springbootservice/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.chexun.springbootservice.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home()
+    {
+        return "Springboot Service is OK!";
+    }
+
+}
